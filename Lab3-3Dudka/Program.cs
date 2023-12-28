5b369fa9e261ec64a4c4b21b262e805f1c495869
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 11:33:35 +0200

    Initial commit

---
+++ Lab3-3Dudka/Program.cs
@@ -0,0 +1,34 @@
+﻿try
+{
+    Console.Write("Введите x: ");
+    double x = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Введите n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    double S = 0;
+
+    for (int i = 1; i <= n; i += 2)
+    {
+        long factorial = 1;
+        for (int j = 1; j <= i; j++)
+        {
+            factorial *= j;
+        }
+
+        if (i % 2 == 0)
+        {
+            S += Math.Pow(x, i) / factorial;
+        }
+        else
+        {
+            S-= Math.Pow(x, i) / factorial;
+        }
+    }
+
+    Console.WriteLine($"S = {S:F2}");
+}
+catch (Exception err)
+{
+    Console.WriteLine(err);
+}
